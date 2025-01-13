d53fee56571e65e88cbae165e75aa33e3c73d1c4
Dm-Rb <dm.rb.418http@gmail.com>
2025-01-13 14:42:18 +0300

    Добавлен генератор текста для хендлера word_details

--- bot/templates/text.py
+++ bot/templates/text.py
@@ -1,4 +1,5 @@
-###
+from bot.services.utils import frequency_word
+
 
 def start_command(user_first_name):
     message = \
@@ -7,3 +8,14 @@ def start_command(user_first_name):
 
     return message
 
+def word_details(word_details_dict):
+    message = ''
+    # Правильное закрытие тега <b>
+    message += f"<b>{word_details_dict['word'].capitalize()}</b>"
+    for item_transl in word_details_dict['translation']:
+        # Используем <i> с корректным закрытием </i>
+        transl_text = f"\n<i>{item_transl['pos_en']}/{item_transl['pos_ru']}:</i>\n"
+        # Формируем список переведенных слов
+        transl_text += ', '.join([f"{item['word']} <i>({frequency_word(item['freq'])})</i>" for item in item_transl['words_list']])
+        message += '\n' + transl_text
+    return message
