5240f2f0a0a22aac4a989d8b98230244775c6efa
Dm-Rb <dm.rb.418http@gmail.com>
2025-01-13 14:44:48 +0300

    Добавлены 3 функции: стак списка по частям речи, конверт ответа local_database в стандартизированный список словарей, букенный эквивалент числовым значениям frequency

--- bot/services/utils.py
+++ bot/services/utils.py
@@ -4,8 +4,22 @@ import re
 """
 В этом модуле содержатся функции для обработки текста и работы с массивами
 """
+def preparing_array_tuple2dict(array: list[tuple]) -> list[dict] or None:
+    if not array:
+        return array
+    result = []
+    for item in array:
+        result.append(
+            {
+                'word_en': item[0],
+                'word_ru': item[1],
+                'pos_en':  item[2],
+                'pos_ru': item[3],
+                'freq': item[4]
 
-
+            }
+        )
+    return result
 def detect_language(text):
     # проверка на наличие только букв и пробелов
     if not re.match("^[а-яА-Яa-zA-Z ]*$", text):
@@ -21,3 +35,89 @@ def detect_language(text):
 
     # Если текст пустой или не содержит букв
     return False
+
+
+def grouping_array_by_pos(word_details: list[dict], lang: str = 'en') -> dict or None:
+    """
+    :return
+    {   'word': str,
+        'lang': str,
+        "translation":
+            [
+                {
+                    "pos_en": 'str',
+                    "pos_ru": 'str',
+                    'words_list': [
+                                        {'word': str, freq': int}
+                                        {...},
+                                        ...
+                                      ]
+                },
+                {...}, ..
+            ]
+    }
+    """
+    # Проверяет валидность аргумента <lang>
+    if lang not in ['en', 'ru']:
+        raise ValueError("Argument <lang> is not valid. It must be in ['en', 'ru']")
+    if not word_details:
+        return
+    try:
+        word_main = word_details[0][f'word_{lang}']
+    except KeyError as _ex:
+        print(_ex)
+        return
+    except IndexError as _ex:
+        print(_ex)
+        return
+
+        # Фильтрует по убыванию "freq"
+    word_details.sort(key=lambda x: x['freq'], reverse=True)
+
+    # Определяем язык перевода
+    transl_lang = 'ru' if lang == 'en' else 'en'
+
+    # Группируем данные
+    grouped = {}
+    for item in word_details:
+        pos_en = item['pos_en']
+        pos_ru = item['pos_ru']
+        word = item[f'word_{transl_lang}']
+        freq = item['freq']
+
+        if pos_en not in grouped:
+            grouped[pos_en] = {
+                'pos_en': pos_en,
+                'pos_ru': pos_ru,
+                'words_list': []
+            }
+
+        grouped[pos_en]['words_list'].append({'word': word, 'freq': freq})
+
+    # Преобразуем словарь в список
+    translation = list(grouped.values())
+    result = {
+        'word': word_main,
+        'lang': lang,
+        'translation': translation
+    }
+    return result
+
+
+def frequency_word(freq: int) -> str:
+    if freq == 1:
+        return 'редко'
+    elif freq in range(2, 5):
+        return 'нечасто'
+    elif freq in range(5, 11):
+        return 'часто'
+    else:
+        return 'оч. часто'
+
+
+#
+# test_l = [{'word_en': 'should', 'word_ru': 'должны', 'pos_en': 'adjective', 'pos_ru': 'прилагательное', 'freq': 10}, {'word_en': 'should', 'word_ru': 'необходимо', 'pos_en': 'predicative', 'pos_ru': 'предикатив', 'freq': 5}, {'word_en': 'should', 'word_ru': 'нужно', 'pos_en': 'predicative', 'pos_ru': 'предикатив', 'freq': 5}, {'word_en': 'should', 'word_ru': 'надо', 'pos_en': 'predicative', 'pos_ru': 'предикатив', 'freq': 5}, {'word_en': 'should', 'word_ru': 'следует', 'pos_en': 'verb', 'pos_ru': 'глагол', 'freq': 10}, {'word_en': 'should', 'word_ru': 'стоит', 'pos_en': 'verb', 'pos_ru': 'глагол', 'freq': 1}, {'word_en': 'should', 'word_ru': 'приходиться', 'pos_en': 'verb', 'pos_ru': 'глагол', 'freq': 1}, {'word_en': 'should', 'word_ru': 'надлежать', 'pos_en': 'verb', 'pos_ru': 'глагол', 'freq': 1}, {'word_en': 'should', 'word_ru': 'если', 'pos_en': 'conjunction', 'pos_ru': 'союз', 'freq': 5}, {'word_en': 'should', 'word_ru': 'бы', 'pos_en': 'particle', 'pos_ru': 'частица', 'freq': 1}]
+# r = grouping_array_by_pos(test_l,  'en')
+# from bot.templates.text import word_details
+#
+# print(word_details(r))
