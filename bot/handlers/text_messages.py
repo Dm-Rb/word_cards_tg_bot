032671899ed2928e3a390ffdfad51c0087669567
Dm-Rb <dm.rb.418http@gmail.com>
2025-01-14 12:02:39 +0300

    Добавлена обработка запроса к апи в случае отсутствия слова в БД

--- bot/handlers/text_messages.py
+++ bot/handlers/text_messages.py
@@ -4,6 +4,7 @@ from bot.keyboards.inline import get_kb__yes_no_answer
 from bot.globals import database
 from bot.services.utils import detect_language, grouping_array_by_pos, preparing_array_tuple2dict
 from bot.templates.text import word_details as preparing_message
+from bot.services.api_clients import ya_dict_api
 
 
 router = Router()
@@ -12,12 +13,12 @@ router = Router()
 @router.message(F.text)  # Хендлер срабатывает на любое текстовое сообщение
 async def user_word_handler(message: Message):
     """
-    Допилить!!!
+    Допилить обработку API на русском!!!
     """
     word = message.text.strip().lower()  # Получаем отправленное слово
     lang = detect_language(word)
     if not lang:
-        await message.answer("Запрос отклонён, строка содержит недопустимые символы (цифры, спецсимволы и т.д.)")
+        await message.answer("Cтрока содержит недопустимые символы (цифры, спецсимволы и т.д.)")
         return
     # Получить id, если это слово есть в базе данных
     word_id = await database.get_row_id_by_value_from_table__words(word, lang)
@@ -34,15 +35,24 @@ async def user_word_handler(message: Message):
         word_details: dict = grouping_array_by_pos(word_details, lang)
         # Генерирует текст на базе шаблона
         await message.answer(text=preparing_message(word_details), parse_mode='HTML')
+    # Слово отсутствует в БД, делаем запрос к API Yandex Dictionary
     else:
-        # Обратиться к api
-        pass
+        print('flag')
+        # Запрос к APi
+        ya_dict_api_resp = await ya_dict_api.get_word_details_from_ya_dict(word, lang)
+        # Если пустой ответ
+        if not ya_dict_api_resp:
+            await message.answer("Слово не найдено, возможно при вводе была допущена опечатка")
+            return
+        # Преобразует список словарей в словарь. Группировка по частям речи (стакает переводы по частям речи)
+        word_details: dict = grouping_array_by_pos(ya_dict_api_resp, lang)
+        await message.answer(text=preparing_message(word_details), parse_mode='HTML')
 
     # Текст из функции check_word
-    reply_text = f"{word}\nДобавить в ваш личный словарь для изучения?"
-
-    # Используем клавиатуру из inline.py
-    keyboard = get_kb__yes_no_answer(word)
-
-    # Отправляем сообщение с клавиатурой
-    await message.answer(reply_text, reply_markup=keyboard)
+    # reply_text = f"{word}\nДобавить в ваш личный словарь для изучения?"
+    #
+    # # Используем клавиатуру из inline.py
+    # keyboard = get_kb__yes_no_answer(word)
+    #
+    # # Отправляем сообщение с клавиатурой
+    # await message.answer(reply_text, reply_markup=keyboard)
