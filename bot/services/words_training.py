2cdf5c673056e29885833c701d511889e76e1abb
Dm-Rb <dm.rb.418http@gmail.com>
2025-01-21 17:19:17 +0300

    test

--- bot/services/words_training.py
+++ bot/services/words_training.py
@@ -5,7 +5,7 @@ import pymorphy2
 class WordsTraining:
 
     # таблица соответствий констант из pymorphy2
-    soe_db_pymorphy2 = {
+    pos_db_pymorphy2 = {
         'noun': 'NOUN',
         'adjective': 'ADJF',
         'participle': 'PRTF',
@@ -14,8 +14,18 @@ class WordsTraining:
         'preposition': 'PREP',
         'conjunction': 'CONJ',
         'interjection': 'INTJ',
-        'determiner': '',
         'particle': 'PRCL',
         'numeral': 'NUMR',
         'predicative': 'PRED'
     }
+
+    def __init__(self):
+        self.morph = pymorphy2.MorphAnalyzer()
+    def get_morphological_analogues_ru(self, word_ru, pos):
+        parsed_word = self.morph.parse(word_ru)[0]  # Берем первую интерпретацию
+
+        forms = {form.word for form in parsed_word.lexeme if form.tag.POS == pos}
+        return forms
+d = WordsTraining()
+r = d.get_morphological_analogues_ru('замок', 'NOUN')
+print(r)
