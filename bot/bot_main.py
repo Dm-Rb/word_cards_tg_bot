7d4ee7bc5bf98a25cea797114f77e86f6ddfd0ac
Dm-Rb <dm.rb.418http@gmail.com>
2025-01-23 17:08:36 +0300

    подключен роутер хендлеров состояния

--- bot/bot_main.py
+++ bot/bot_main.py
@@ -2,7 +2,7 @@ from config_file import config
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
-from bot.handlers import commands, text_messages, callback_query
+from bot.handlers import commands, messages, callbacks, states
 from bot.globals import database
 
 
@@ -16,8 +16,11 @@ async def main():
 
     # подключаем роутеры к диспетчеру
     dp.include_router(commands.router)
-    dp.include_router(text_messages.router)
-    dp.include_router(callback_query.router)
+    dp.include_router(states.router)
+    dp.include_router(messages.router)
+    dp.include_router(callbacks.router)
+
+
 
     # # Регистрируем хендлеры
     # setup_handlers(dp)
